56acf2d03420fed668efbdddd6da1179fc503439
Steven Oyer <contact.stevenoyer@gmail.com>
2024-02-09 13:38:54 +0100

    Upgrade to 6.0

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -12,6 +12,13 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login', methods: ['POST', 'GET'])]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        /**
+         * Redirect user if logged
+         */
+        if ($this->getUser()) {
+            return $this->redirectToRoute('homepage');
+        }
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
